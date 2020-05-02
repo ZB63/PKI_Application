f1f729a43753c030e05d01e72411880d7f35f91d
Sebastian.Paweloszek <spaweloszek63@gmail.com>
2020-05-02 13:18:51 +0200

    jsp test

--- src/main/java/com/pki/PKI/controller/SimpleController.java
+++ src/main/java/com/pki/PKI/controller/SimpleController.java
@@ -1,21 +1,29 @@
 package com.pki.PKI.controller;
 
+import com.pki.PKI.domain.User;
+import com.pki.PKI.repository.UserRepository;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.security.Principal;
+import java.util.List;
 
 @RestController
 public class SimpleController {
 
-    @GetMapping(value = "/user")
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping(value = "/content")
     public String user(Principal principal) throws IOException {
 
         ObjectMapper mapper = new ObjectMapper();
@@ -24,8 +32,15 @@ public class SimpleController {
         int index2 = user.indexOf("verified_email");
 
         String email = user.substring(index1+8,index2-3);
+        List<User> users = userRepository.findAll();
 
-        return "Zalogowano jako: " + email + "<br/><a href = \"logout\">Wyloguj!</a>";
+        ModelAndView model = new ModelAndView("content");
+
+        model.addObject("email", email);
+        model.addObject("users", users);
+
+        //return "Zalogowano jako: " + email + "<br/><a href = \"logout\">Wyloguj!</a>";
+        return "redirect:/content";
     }
 
     @GetMapping(value="/logout")
